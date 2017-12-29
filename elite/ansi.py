e786fb69a30c53b12c12fa226b0e2a84d6d2a4fa
Fotis Gimian <fgimiansoftware@gmail.com>
2017-12-30 08:53:01 +1100

    Cleaned up and simplified ANSI style and colour library

--- elite/ansi.py
+++ elite/ansi.py
@@ -1,10 +1,15 @@
-# Colours
+# Text Styles
 BOLD = '\x1b[1m'
+ITALIC = '\x1b[3m'
 UNDERLINE = '\x1b[4m'
+
+# Bright Colours
 RED = '\x1b[91m'
 GREEN = '\x1b[92m'
 YELLOW = '\x1b[93m'
 BLUE = '\x1b[94m'
+PURPLE = '\x1b[95m'
+CYAN = '\x1b[96m'
 WHITE = '\x1b[97m'
 ENDC = '\x1b[0m'
 
@@ -28,52 +33,3 @@ def move_forward(num_columns):
 
 def move_backward(num_columns):
     return f'\033[{num_columns}D' if num_columns else ''
-
-
-# TODO: clean me please
-# 'reset': (0, 0),
-
-# 'bold': (1, 22),
-# 'faint': (2, 22),
-# 'italic': (3, 23),
-# 'underline': (4, 24),
-# 'blinking': (5, 25),
-# 'inverse': (7, 27),
-# 'invisible': (8, 28),
-# 'strikethrough': (9, 29),
-
-# 'black': (30, 39),
-# 'red': (31, 39),
-# 'green': (32, 39),
-# 'yellow': (33, 39),
-# 'blue': (34, 39),
-# 'magenta': (35, 39),
-# 'cyan': (36, 39),
-# 'white': (37, 39),
-
-# 'light_black': (90, 39),
-# 'light_red': (91, 39),
-# 'light_green': (92, 39),
-# 'light_yellow': (93, 39),
-# 'light_blue': (94, 39),
-# 'light_magenta': (95, 39),
-# 'light_cyan': (96, 39),
-# 'light_white': (97, 39),
-
-# 'on_black': (40, 49),
-# 'on_red': (41, 49),
-# 'on_green': (42, 49),
-# 'on_yellow': (43, 49),
-# 'on_blue': (44, 49),
-# 'on_magenta': (45, 49),
-# 'on_cyan': (46, 49),
-# 'on_white': (47, 49),
-
-# 'on_light_black': (100, 49),
-# 'on_light_red': (101, 49),
-# 'on_light_green': (102, 49),
-# 'on_light_yellow': (103, 49),
-# 'on_light_blue': (104, 49),
-# 'on_light_magenta': (105, 49),
-# 'on_light_cyan': (106, 49),
-# 'on_light_white': (107, 49)
