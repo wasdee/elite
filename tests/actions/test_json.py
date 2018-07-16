ba31d8a1fbb848b64418c8d5738819b5d0c11eb2
Fotis Gimian <fgimiansoftware@gmail.com>
2018-07-16 23:15:35 +1000

    Switched to using the pytest tmpdir fixture for json and spotify tests instead of using tempfile

--- tests/actions/test_json.py
+++ tests/actions/test_json.py
@@ -1,5 +1,3 @@
-import os
-import tempfile
 import textwrap
 
 import pytest
@@ -7,115 +5,94 @@ from elite.actions import ActionError, ActionResponse
 from elite.actions.json import JSON
 
 
-def test_json_same(monkeypatch):
-    with tempfile.NamedTemporaryFile('w', delete=False) as fp:
-        fp.write(textwrap.dedent('''\
-            {
-                "name": "Fots",
-                "python_lover": true,
-                "interests": ["chickens", "coding", "music"]
-            }
-        '''))
+def test_json_same(tmpdir):
+    p = tmpdir.join('test.json')
+    p.write(textwrap.dedent('''\
+        {
+            "name": "Fots",
+            "python_lover": true,
+            "interests": ["chickens", "coding", "music"]
+        }
+    '''))
 
-    try:
-        json = JSON(path=fp.name, values={'python_lover': True})
-        assert json.process() == ActionResponse(changed=False, data={'path': fp.name})
-
-        with open(fp.name, 'r') as fp:
-            assert fp.read() == textwrap.dedent('''\
-                {
-                    "name": "Fots",
-                    "python_lover": true,
-                    "interests": ["chickens", "coding", "music"]
-                }
-            ''')
-    finally:
-        os.remove(fp.name)
+    json = JSON(path=p.strpath, values={'python_lover': True})
+    assert json.process() == ActionResponse(changed=False, data={'path': p.strpath})
+    assert p.read() == textwrap.dedent('''\
+        {
+            "name": "Fots",
+            "python_lover": true,
+            "interests": ["chickens", "coding", "music"]
+        }
+    ''')
 
 
-def test_json_different(monkeypatch):
-    with tempfile.NamedTemporaryFile('w', delete=False) as fp:
-        fp.write(textwrap.dedent('''\
-            {
-                "name": "Fots",
-                "python_lover": true,
-                "interests": ["chickens", "coding", "music"]
-            }
-        '''))
+def test_json_different(tmpdir):
+    p = tmpdir.join('test.json')
+    p.write(textwrap.dedent('''\
+        {
+            "name": "Fots",
+            "python_lover": true,
+            "interests": ["chickens", "coding", "music"]
+        }
+    '''))
 
-    try:
-        json = JSON(path=fp.name, values={'python_lover': False})
-        assert json.process() == ActionResponse(changed=True, data={'path': fp.name})
-
-        with open(fp.name, 'r') as fp:
-            assert fp.read() == textwrap.dedent('''\
-                {
-                  "name": "Fots",
-                  "python_lover": false,
-                  "interests": [
-                    "chickens",
-                    "coding",
-                    "music"
-                  ]
-                }
-            ''')
-    finally:
-        os.remove(fp.name)
+    json = JSON(path=p.strpath, values={'python_lover': False})
+    assert json.process() == ActionResponse(changed=True, data={'path': p.strpath})
+    assert p.read() == textwrap.dedent('''\
+        {
+          "name": "Fots",
+          "python_lover": false,
+          "interests": [
+            "chickens",
+            "coding",
+            "music"
+          ]
+        }
+    ''')
 
 
-def test_json_inexistent_values(monkeypatch):
-    with tempfile.NamedTemporaryFile('w', delete=False) as fp:
-        fp.write(textwrap.dedent('''\
-            {
-                "name": "Fots"
-            }
-        '''))
+def test_json_inexistent_values(tmpdir):
+    p = tmpdir.join('test.json')
+    p.write(textwrap.dedent('''\
+        {
+            "name": "Fots"
+        }
+    '''))
 
-    try:
-        json = JSON(path=fp.name, values={'python_lover': False})
-        assert json.process() == ActionResponse(changed=True, data={'path': fp.name})
-
-        with open(fp.name, 'r') as fp:
-            assert fp.read() == textwrap.dedent('''\
-                {
-                  "name": "Fots",
-                  "python_lover": false
-                }
-            ''')
-    finally:
-        os.remove(fp.name)
+    json = JSON(path=p.strpath, values={'python_lover': False})
+    assert json.process() == ActionResponse(changed=True, data={'path': p.strpath})
+    assert p.read() == textwrap.dedent('''\
+        {
+          "name": "Fots",
+          "python_lover": false
+        }
+    ''')
 
 
-def test_json_inexistent_path(monkeypatch):
-    with tempfile.TemporaryDirectory() as temp_dir:
-        path = os.path.join(temp_dir, 'temp.json')
+def test_json_inexistent_path(tmpdir):
+    p = tmpdir.join('test.json')
 
-        json = JSON(path=path, values={'python_lover': False})
-        assert json.process() == ActionResponse(changed=True, data={'path': path})
-
-        with open(path, 'r') as fp:
-            assert fp.read() == textwrap.dedent('''\
-                {
-                  "python_lover": false
-                }
-            ''')
+    json = JSON(path=p.strpath, values={'python_lover': False})
+    assert json.process() == ActionResponse(changed=True, data={'path': p.strpath})
+    assert p.read() == textwrap.dedent('''\
+        {
+          "python_lover": false
+        }
+    ''')
 
 
-def test_json_invalid_file_parsing(monkeypatch):
-    with tempfile.NamedTemporaryFile('w', delete=False) as fp:
-        fp.write(textwrap.dedent('''\
-            hmmmm
-        '''))
+def test_json_invalid_file_parsing(tmpdir):
+    p = tmpdir.join('test.json')
+    p.write(textwrap.dedent('''\
+        hmmmm
+    '''))
 
-    try:
-        json = JSON(path=fp.name, values={'name': 'Fots'})
-        with pytest.raises(ActionError):
-            json.process()
-    finally:
-        os.remove(fp.name)
+    json = JSON(path=p.strpath, values={'name': 'Fots'})
+    with pytest.raises(ActionError):
+        json.process()
 
 
-def test_prefs_not_writable(monkeypatch):
+def test_prefs_not_writable():
     json = JSON(path='/', values={'name': 'Fots'})
     with pytest.raises(ActionError):
         json.process()
--- tests/actions/test_spotify.py
+++ tests/actions/test_spotify.py
@@ -1,5 +1,3 @@
-import os
-import tempfile
 import textwrap
 
 import pytest
@@ -21,311 +19,257 @@ def test_determine_pref_path_username():
     )
 
 
-def test_pref_boolean_same(monkeypatch):
-    with tempfile.NamedTemporaryFile('w', delete=False) as fp:
-        fp.write(textwrap.dedent('''\
-            audio.sync_bitrate_enumeration=3
-            audio.play_bitrate_enumeration=0
-            ui.hide_hpto=true
-            audio.normalize_v2=false
-            app.player.autoplay=false
-            ui.show_friend_feed=false
-        '''))
+def test_pref_boolean_same(tmpdir, monkeypatch):
+    p = tmpdir.join('prefs')
+    p.write(textwrap.dedent('''\
+        audio.sync_bitrate_enumeration=3
+        audio.play_bitrate_enumeration=0
+        ui.hide_hpto=true
+        audio.normalize_v2=false
+        app.player.autoplay=false
+        ui.show_friend_feed=false
+    '''))
 
-    monkeypatch.setattr(Spotify, 'determine_pref_path', lambda self: fp.name)
+    monkeypatch.setattr(Spotify, 'determine_pref_path', lambda self: p.strpath)
 
-    try:
-        spotify = Spotify(values={'ui.show_friend_feed': False})
-        assert spotify.process() == ActionResponse(changed=False, data={'path': fp.name})
-
-        with open(fp.name, 'r') as fp:
-            assert fp.read() == textwrap.dedent('''\
-                audio.sync_bitrate_enumeration=3
-                audio.play_bitrate_enumeration=0
-                ui.hide_hpto=true
-                audio.normalize_v2=false
-                app.player.autoplay=false
-                ui.show_friend_feed=false
-            ''')
-    finally:
-        os.remove(fp.name)
+    spotify = Spotify(values={'ui.show_friend_feed': False})
+    assert spotify.process() == ActionResponse(changed=False, data={'path': p.strpath})
+    assert p.read() == textwrap.dedent('''\
+        audio.sync_bitrate_enumeration=3
+        audio.play_bitrate_enumeration=0
+        ui.hide_hpto=true
+        audio.normalize_v2=false
+        app.player.autoplay=false
+        ui.show_friend_feed=false
+    ''')
 
 
-def test_pref_invalid_supplied_value(monkeypatch):
-    with tempfile.NamedTemporaryFile('w', delete=False) as fp:
-        fp.write(textwrap.dedent('''\
-            audio.sync_bitrate_enumeration=3
-            audio.play_bitrate_enumeration=0
-            ui.hide_hpto=true
-            audio.normalize_v2=false
-            app.player.autoplay=false
-            ui.show_friend_feed=true
-        '''))
+def test_pref_invalid_supplied_value(tmpdir, monkeypatch):
+    p = tmpdir.join('prefs')
+    p.write(textwrap.dedent('''\
+        audio.sync_bitrate_enumeration=3
+        audio.play_bitrate_enumeration=0
+        ui.hide_hpto=true
+        audio.normalize_v2=false
+        app.player.autoplay=false
+        ui.show_friend_feed=true
+    '''))
 
-    monkeypatch.setattr(Spotify, 'determine_pref_path', lambda self: fp.name)
+    monkeypatch.setattr(Spotify, 'determine_pref_path', lambda self: p.strpath)
 
-    try:
-        spotify = Spotify(values={'wow': 5.5})
-        with pytest.raises(ActionError):
-            spotify.process()
-    finally:
-        os.remove(fp.name)
+    spotify = Spotify(values={'wow': 5.5})
+    with pytest.raises(ActionError):
+        spotify.process()
 
 
-def test_pref_boolean_different(monkeypatch):
-    with tempfile.NamedTemporaryFile('w', delete=False) as fp:
-        fp.write(textwrap.dedent('''\
-            audio.sync_bitrate_enumeration=3
-            audio.play_bitrate_enumeration=0
-            ui.hide_hpto=true
-            audio.normalize_v2=false
-            app.player.autoplay=false
-            ui.show_friend_feed=true
-        '''))
+def test_pref_boolean_different(tmpdir, monkeypatch):
+    p = tmpdir.join('prefs')
+    p.write(textwrap.dedent('''\
+        audio.sync_bitrate_enumeration=3
+        audio.play_bitrate_enumeration=0
+        ui.hide_hpto=true
+        audio.normalize_v2=false
+        app.player.autoplay=false
+        ui.show_friend_feed=true
+    '''))
 
-    monkeypatch.setattr(Spotify, 'determine_pref_path', lambda self: fp.name)
+    monkeypatch.setattr(Spotify, 'determine_pref_path', lambda self: p.strpath)
 
-    try:
-        spotify = Spotify(values={'ui.show_friend_feed': False})
-        assert spotify.process() == ActionResponse(changed=True, data={'path': fp.name})
-
-        with open(fp.name, 'r') as fp:
-            assert fp.read() == textwrap.dedent('''\
-                audio.sync_bitrate_enumeration=3
-                audio.play_bitrate_enumeration=0
-                ui.hide_hpto=true
-                audio.normalize_v2=false
-                app.player.autoplay=false
-                ui.show_friend_feed=false
-            ''')
-    finally:
-        os.remove(fp.name)
+    spotify = Spotify(values={'ui.show_friend_feed': False})
+    assert spotify.process() == ActionResponse(changed=True, data={'path': p.strpath})
+    assert p.read() == textwrap.dedent('''\
+        audio.sync_bitrate_enumeration=3
+        audio.play_bitrate_enumeration=0
+        ui.hide_hpto=true
+        audio.normalize_v2=false
+        app.player.autoplay=false
+        ui.show_friend_feed=false
+    ''')
 
 
-def test_pref_boolean_inexistent(monkeypatch):
-    fp = tempfile.NamedTemporaryFile('w', delete=False)
+def test_pref_boolean_inexistent(tmpdir, monkeypatch):
+    p = tmpdir.join('prefs').ensure()
 
-    monkeypatch.setattr(Spotify, 'determine_pref_path', lambda self: fp.name)
+    monkeypatch.setattr(Spotify, 'determine_pref_path', lambda self: p.strpath)
 
-    try:
-        spotify = Spotify(values={'ui.show_friend_feed': False})
-        assert spotify.process() == ActionResponse(changed=True, data={'path': fp.name})
-
-        with open(fp.name, 'r') as fp:
-            assert fp.read() == textwrap.dedent('''\
-                ui.show_friend_feed=false
-            ''')
-    finally:
-        os.remove(fp.name)
+    spotify = Spotify(values={'ui.show_friend_feed': False})
+    assert spotify.process() == ActionResponse(changed=True, data={'path': p.strpath})
+    assert p.read() == textwrap.dedent('''\
+        ui.show_friend_feed=false
+    ''')
 
 
-def test_pref_integer_same(monkeypatch):
-    with tempfile.NamedTemporaryFile('w', delete=False) as fp:
-        fp.write(textwrap.dedent('''\
-            audio.sync_bitrate_enumeration=4
-            audio.play_bitrate_enumeration=4
-            ui.hide_hpto=true
-            audio.normalize_v2=false
-            app.player.autoplay=false
-            ui.show_friend_feed=false
-        '''))
+def test_pref_integer_same(tmpdir, monkeypatch):
+    p = tmpdir.join('prefs')
+    p.write(textwrap.dedent('''\
+        audio.sync_bitrate_enumeration=4
+        audio.play_bitrate_enumeration=4
+        ui.hide_hpto=true
+        audio.normalize_v2=false
+        app.player.autoplay=false
+        ui.show_friend_feed=false
+    '''))
 
-    monkeypatch.setattr(Spotify, 'determine_pref_path', lambda self: fp.name)
+    monkeypatch.setattr(Spotify, 'determine_pref_path', lambda self: p.strpath)
 
-    try:
-        spotify = Spotify(values={'audio.sync_bitrate_enumeration': 4}, username='fots')
-        assert spotify.process() == ActionResponse(changed=False, data={'path': fp.name})
-
-        with open(fp.name, 'r') as fp:
-            assert fp.read() == textwrap.dedent('''\
-                audio.sync_bitrate_enumeration=4
-                audio.play_bitrate_enumeration=4
-                ui.hide_hpto=true
-                audio.normalize_v2=false
-                app.player.autoplay=false
-                ui.show_friend_feed=false
-            ''')
-    finally:
-        os.remove(fp.name)
+    spotify = Spotify(values={'audio.sync_bitrate_enumeration': 4}, username='fots')
+    assert spotify.process() == ActionResponse(changed=False, data={'path': p.strpath})
+    assert p.read() == textwrap.dedent('''\
+        audio.sync_bitrate_enumeration=4
+        audio.play_bitrate_enumeration=4
+        ui.hide_hpto=true
+        audio.normalize_v2=false
+        app.player.autoplay=false
+        ui.show_friend_feed=false
+    ''')
 
 
-def test_pref_integer_different(monkeypatch):
-    with tempfile.NamedTemporaryFile('w', delete=False) as fp:
-        fp.write(textwrap.dedent('''\
-            audio.sync_bitrate_enumeration=3
-            audio.play_bitrate_enumeration=0
-            ui.hide_hpto=true
-            audio.normalize_v2=false
-            app.player.autoplay=false
-            ui.show_friend_feed=false
-        '''))
+def test_pref_integer_different(tmpdir, monkeypatch):
+    p = tmpdir.join('prefs')
+    p.write(textwrap.dedent('''\
+        audio.sync_bitrate_enumeration=3
+        audio.play_bitrate_enumeration=0
+        ui.hide_hpto=true
+        audio.normalize_v2=false
+        app.player.autoplay=false
+        ui.show_friend_feed=false
+    '''))
 
-    monkeypatch.setattr(Spotify, 'determine_pref_path', lambda self: fp.name)
+    monkeypatch.setattr(Spotify, 'determine_pref_path', lambda self: p.strpath)
 
-    try:
-        spotify = Spotify(
-            values={
-                'audio.sync_bitrate_enumeration': 4,
-                'audio.play_bitrate_enumeration': 4
-            },
-            username='fots'
-        )
-        assert spotify.process() == ActionResponse(changed=True, data={'path': fp.name})
-
-        with open(fp.name, 'r') as fp:
-            assert fp.read() == textwrap.dedent('''\
-                audio.sync_bitrate_enumeration=4
-                audio.play_bitrate_enumeration=4
-                ui.hide_hpto=true
-                audio.normalize_v2=false
-                app.player.autoplay=false
-                ui.show_friend_feed=false
-            ''')
-    finally:
-        os.remove(fp.name)
+    spotify = Spotify(
+        values={
+            'audio.sync_bitrate_enumeration': 4,
+            'audio.play_bitrate_enumeration': 4
+        },
+        username='fots'
+    )
+    assert spotify.process() == ActionResponse(changed=True, data={'path': p.strpath})
+    assert p.read() == textwrap.dedent('''\
+        audio.sync_bitrate_enumeration=4
+        audio.play_bitrate_enumeration=4
+        ui.hide_hpto=true
+        audio.normalize_v2=false
+        app.player.autoplay=false
+        ui.show_friend_feed=false
+    ''')
 
 
-def test_pref_integer_inexistent(monkeypatch):
-    fp = tempfile.NamedTemporaryFile('w', delete=False)
+def test_pref_integer_inexistent(tmpdir, monkeypatch):
+    p = tmpdir.join('prefs').ensure()
 
-    monkeypatch.setattr(Spotify, 'determine_pref_path', lambda self: fp.name)
+    monkeypatch.setattr(Spotify, 'determine_pref_path', lambda self: p.strpath)
 
-    try:
-        spotify = Spotify(values={'audio.sync_bitrate_enumeration': 4}, username='fots')
-        assert spotify.process() == ActionResponse(changed=True, data={'path': fp.name})
-
-        with open(fp.name, 'r') as fp:
-            assert fp.read() == textwrap.dedent('''\
-                audio.sync_bitrate_enumeration=4
-            ''')
-    finally:
-        os.remove(fp.name)
+    spotify = Spotify(values={'audio.sync_bitrate_enumeration': 4}, username='fots')
+    assert spotify.process() == ActionResponse(changed=True, data={'path': p.strpath})
+    assert p.read() == textwrap.dedent('''\
+        audio.sync_bitrate_enumeration=4
+    ''')
 
 
-def test_pref_string_same(monkeypatch):
-    with tempfile.NamedTemporaryFile('w', delete=False) as fp:
-        fp.write(textwrap.dedent('''\
-            autologin.canonical_username="fots"
-            autologin.username="fots"
-            app.autostart-mode="off"
-            app.autostart-banner-seen=true
-            core.clock_delta=-1
-            app.autostart-configured=true
-        '''))
+def test_pref_string_same(tmpdir, monkeypatch):
+    p = tmpdir.join('prefs')
+    p.write(textwrap.dedent('''\
+        autologin.canonical_username="fots"
+        autologin.username="fots"
+        app.autostart-mode="off"
+        app.autostart-banner-seen=true
+        core.clock_delta=-1
+        app.autostart-configured=true
+    '''))
 
-    monkeypatch.setattr(Spotify, 'determine_pref_path', lambda self: fp.name)
+    monkeypatch.setattr(Spotify, 'determine_pref_path', lambda self: p.strpath)
 
-    try:
-        spotify = Spotify(
-            values={
-                'autologin.canonical_username': 'fots',
-                'autologin.username': 'fots'
-            }
-        )
-        assert spotify.process() == ActionResponse(changed=False, data={'path': fp.name})
-
-        with open(fp.name, 'r') as fp:
-            assert fp.read() == textwrap.dedent('''\
-                autologin.canonical_username="fots"
-                autologin.username="fots"
-                app.autostart-mode="off"
-                app.autostart-banner-seen=true
-                core.clock_delta=-1
-                app.autostart-configured=true
-            ''')
-    finally:
-        os.remove(fp.name)
+    spotify = Spotify(
+        values={
+            'autologin.canonical_username': 'fots',
+            'autologin.username': 'fots'
+        }
+    )
+    assert spotify.process() == ActionResponse(changed=False, data={'path': p.strpath})
+    assert p.read() == textwrap.dedent('''\
+        autologin.canonical_username="fots"
+        autologin.username="fots"
+        app.autostart-mode="off"
+        app.autostart-banner-seen=true
+        core.clock_delta=-1
+        app.autostart-configured=true
+    ''')
 
 
-def test_pref_string_different(monkeypatch):
-    with tempfile.NamedTemporaryFile('w', delete=False) as fp:
-        fp.write(textwrap.dedent('''\
-            autologin.canonical_username="pablo"
-            autologin.username="pablo"
-            app.autostart-mode="off"
-            app.autostart-banner-seen=true
-            core.clock_delta=-1
-            app.autostart-configured=true
-        '''))
+def test_pref_string_different(tmpdir, monkeypatch):
+    p = tmpdir.join('prefs')
+    p.write(textwrap.dedent('''\
+        autologin.canonical_username="pablo"
+        autologin.username="pablo"
+        app.autostart-mode="off"
+        app.autostart-banner-seen=true
+        core.clock_delta=-1
+        app.autostart-configured=true
+    '''))
 
-    monkeypatch.setattr(Spotify, 'determine_pref_path', lambda self: fp.name)
+    monkeypatch.setattr(Spotify, 'determine_pref_path', lambda self: p.strpath)
 
-    try:
-        spotify = Spotify(
-            values={
-                'autologin.canonical_username': 'fots',
-                'autologin.username': 'fots'
-            }
-        )
-        assert spotify.process() == ActionResponse(changed=True, data={'path': fp.name})
-
-        with open(fp.name, 'r') as fp:
-            assert fp.read() == textwrap.dedent('''\
-                autologin.canonical_username="fots"
-                autologin.username="fots"
-                app.autostart-mode="off"
-                app.autostart-banner-seen=true
-                core.clock_delta=-1
-                app.autostart-configured=true
-            ''')
-    finally:
-        os.remove(fp.name)
+    spotify = Spotify(
+        values={
+            'autologin.canonical_username': 'fots',
+            'autologin.username': 'fots'
+        }
+    )
+    assert spotify.process() == ActionResponse(changed=True, data={'path': p.strpath})
+    assert p.read() == textwrap.dedent('''\
+        autologin.canonical_username="fots"
+        autologin.username="fots"
+        app.autostart-mode="off"
+        app.autostart-banner-seen=true
+        core.clock_delta=-1
+        app.autostart-configured=true
+    ''')
 
 
-def test_pref_string_inexistent(monkeypatch):
-    fp = tempfile.NamedTemporaryFile('w', delete=False)
+def test_pref_string_inexistent(tmpdir, monkeypatch):
+    p = tmpdir.join('prefs').ensure()
 
-    monkeypatch.setattr(Spotify, 'determine_pref_path', lambda self: fp.name)
+    monkeypatch.setattr(Spotify, 'determine_pref_path', lambda self: p.strpath)
 
-    try:
-        spotify = Spotify(values={'autologin.username': 'fots'})
-        assert spotify.process() == ActionResponse(changed=True, data={'path': fp.name})
-
-        with open(fp.name, 'r') as fp:
-            assert fp.read() == textwrap.dedent('''\
-                autologin.username="fots"
-            ''')
-    finally:
-        os.remove(fp.name)
+    spotify = Spotify(values={'autologin.username': 'fots'})
+    assert spotify.process() == ActionResponse(changed=True, data={'path': p.strpath})
+    assert p.read() == textwrap.dedent('''\
+        autologin.username="fots"
+    ''')
 
 
-def test_prefs_invalid_file_parsing(monkeypatch):
-    with tempfile.NamedTemporaryFile('w', delete=False) as fp:
-        fp.write(textwrap.dedent('''\
-            hmmmm
-        '''))
+def test_prefs_invalid_file_parsing(tmpdir, monkeypatch):
+    p = tmpdir.join('prefs')
+    p.write(textwrap.dedent('''\
+        hmmmm
+    '''))
 
-    monkeypatch.setattr(Spotify, 'determine_pref_path', lambda self: fp.name)
+    monkeypatch.setattr(Spotify, 'determine_pref_path', lambda self: p.strpath)
 
-    try:
-        spotify = Spotify(values={'autologin.username': 'fots'})
-        with pytest.raises(ActionError):
-            spotify.process()
-    finally:
-        os.remove(fp.name)
+    spotify = Spotify(values={'autologin.username': 'fots'})
+    with pytest.raises(ActionError):
+        spotify.process()
 
 
-def test_pref_invalid_file_value(monkeypatch):
-    with tempfile.NamedTemporaryFile('w', delete=False) as fp:
-        fp.write(textwrap.dedent('''\
-            autologin.canonical_username="pablo"
-            autologin.username="pablo"
-            wow=[]
-            app.autostart-configured=true
-        '''))
+def test_pref_invalid_file_value(tmpdir, monkeypatch):
+    p = tmpdir.join('prefs')
+    p.write(textwrap.dedent('''\
+        autologin.canonical_username="pablo"
+        autologin.username="pablo"
+        wow=[]
+        app.autostart-configured=true
+    '''))
 
-    monkeypatch.setattr(Spotify, 'determine_pref_path', lambda self: fp.name)
+    monkeypatch.setattr(Spotify, 'determine_pref_path', lambda self: p.strpath)
 
-    try:
-        spotify = Spotify(values={
-                'autologin.canonical_username': 'fots',
-                'autologin.username': 'fots'
-            }
-        )
-        with pytest.raises(ActionError):
-            spotify.process()
-    finally:
-        os.remove(fp.name)
+    spotify = Spotify(values={
+            'autologin.canonical_username': 'fots',
+            'autologin.username': 'fots'
+        }
+    )
+    with pytest.raises(ActionError):
+        spotify.process()
 
 
 def test_prefs_not_writable(monkeypatch):
