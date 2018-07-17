65874942eaa668feb7ae3ad0e6abf8f58def09ae
Fotis Gimian <fgimiansoftware@gmail.com>
2018-07-17 11:21:30 +1000

    Improved parameter validation in the plist action and ensured that ValueError is being returned

--- elite/actions/plist.py
+++ elite/actions/plist.py
@@ -27,11 +27,11 @@ class Plist(FileAction):
     @domain.setter
     def domain(self, domain):
         if not domain and not self.path:
-            raise ActionError("you must provide either the 'domain' or 'path' argument")
+            raise ValueError("you must provide either the 'domain' or 'path' argument")
         if domain and self.path:
-            raise ActionError("you may only provide one of the 'domain' or 'path' arguments")
+            raise ValueError("you may only provide one of the 'domain' or 'path' arguments")
         if self.container and domain in ['NSGlobalDomain', 'Apple Global Domain']:
-            raise ActionError(
+            raise ValueError(
                 "the 'container' argument is not allowed when updating the global domain"
             )
         self._domain = domain
@@ -42,8 +42,12 @@ class Plist(FileAction):
 
     @container.setter
     def container(self, container):
+        if not self.domain:
+            raise ValueError(
+                "the 'domain' argument is required when specifying the container argument"
+            )
         if container and self.domain in ['NSGlobalDomain', 'Apple Global Domain']:
-            raise ActionError(
+            raise ValueError(
                 "the 'container' argument is not allowed when updating the global domain"
             )
         self._container = container
@@ -55,26 +59,29 @@ class Plist(FileAction):
     @path.setter
     def path(self, path):
         if not self.domain and not path:
-            raise ActionError("you must provide either the 'domain' or 'path' argument")
+            raise ValueError("you must provide either the 'domain' or 'path' argument")
         if self.domain and path:
-            raise ActionError("you may only provide one of the 'domain' or 'path' arguments")
+            raise ValueError("you may only provide one of the 'domain' or 'path' arguments")
         self._path = path
 
-    def process(self):
-        # Determine the path of the plist if the domain was provided
+    def determine_plist_path(self):
+        """Determine the path of the plist using the domain or container provided."""
         if self.domain:
             if self.domain in ['NSGlobalDomain', 'Apple Global Domain']:
-                path = '~/Library/Preferences/.GlobalPreferences.plist'
+                return '~/Library/Preferences/.GlobalPreferences.plist'
             elif self.domain and self.container:
-                path = (
+                return (
                     f'~/Library/Containers/{self.container}/Data/Library/Preferences/'
                     f'{self.domain}.plist'
                 )
             else:
-                path = f'~/Library/Preferences/{self.domain}.plist'
+                return f'~/Library/Preferences/{self.domain}.plist'
+        else:
+            return self.path
 
+    def process(self):
         # Ensure that home directories are taken into account
-        path = os.path.expanduser(path)
+        path = os.path.expanduser(self.determine_plist_path())
 
         # Load the plist or create a fresh data structure if it doesn't exist
         try:
